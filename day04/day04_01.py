fa2e4d03abbf826a3c3e4bf26b89d6b92fc2f017
ding <mydingyan@outlook.com>
2022-08-27 14:19:03 +0800

    Default Changelist

--- day04/day04_01.py
+++ day04/day04_01.py
@@ -45,19 +45,77 @@ print(str_5,str_6)
 
 print(str_5 * 10)
 
-print("=====jion 连接其他 字符串 ======")
+print("=====join 连接其他 字符串 ======")
 
 # join  用自己来连接其他 字符串
 
 # 案例
 print(" ".join(["Hello","Python","world!"]))
 print("_".join(["Hello","Python","world!"]))
-print("-".join(["Hello","Python","world!"]))
+print("-".join(["Hello","Python","world!"]))   #.join表示给前面的字符串赋值
 print("lebo".join(["Hello","Python","world!"]))
 
+
+print("=====字符串性质判断 ======")
+
 # 字符串性质判断
-age = input("请输入年龄：")
-if age.isdigit():
-    print("您输入的年龄是:",age)
-else:
-    print("年龄只能输入纯数字！")
+
+print("=====字符串性质判断案例一：判断是不是纯数字 ======")
+
+# age = input("请输入年龄：")
+# if age.isdigit():
+#     print("您输入的年龄是:",age)
+# else:
+#     print("年龄只能输入纯数字！")
+
+print("=====字符串性质判断案例二：判断是不是纯字母 ======")
+
+print("leaaa".isalpha())    #判断是不是纯字母，返回True
+
+print("=====字符串性质判断案例三：判断字符串是不是全小写 ======")
+# 只要不包含大写，就返回True
+print("leaaa111".islower())  # 返回True
+print("leaaa".islower())  # 返回True
+
+print("=====字符串性质判断案例四：判断字符串是不是全大写 ======")
+print("WZRTYFSD".isupper())   # 返回True
+print("WZRTYFSDawe".isupper())   # False
+
+print("=====字符串性质判断案例五：判断是不是某字符串开头 ======")
+
+print("www.a.yansay.cn".startswith('www'))  # True
+print("www.a.yansay.cn".startswith('http'))    # False
+
+
+print("=====字符串性质判断案例五：判断是不是某字符串结尾 ======")
+
+print("www.a.yansay.cn".endswith('.cn'))    # True
+print("www.a.yansay.cn".endswith('.ca'))    # False
+
+
+# 字符串查找与替换
+str_8 = 'wangzherongyaoautotest'
+
+
+print("=====案例一：字符串出现的次数：======")
+print(str_8.count("a"))  #3次
+print(str_8.count("t"))
+
+print("=====案例二：查找字符串起始索引值（找不到就是-1）======")
+str_8 = 'wangzherongyaoautotest'
+
+print(str_8.find("auto"))   #14，起始索引
+print(str_8.find("autoa"))   #找不到返回-1
+
+print("=====案例二：查找字符串起始索引值（找不到控制台报错）======")
+print(str_8.index("auto"))   #14，起始索引
+# print(str_8.index("auto1"))   # 这个方法找不到时控制台报错ValueError: substring not found
+
+
+print("=====案例三：字符串替换======")
+print(str_8.replace("a","A"))  #替换所有A的字符，返回替换后的字符串
+print(str_8.replace("auto","Auto"))
+
+
+
+# 分切与连接
---
+++ day04/day04_02.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 _*-
+"""
+@author:Yan-PC
+@file: day04_02.py
+@time: 2022/8/27 13:35
+"""
+
