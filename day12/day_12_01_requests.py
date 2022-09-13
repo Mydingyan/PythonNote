45ec00fe587bcb1978af36b4f7b40372aaa7700d
ding <mydingyan@outlook.com>
2022-09-13 19:29:14 +0800

    requests

--- day12/day_12_01_requests.py
+++ day12/day_12_01_requests.py
@@ -41,9 +41,31 @@ import requests
 
 
 response = requests.get("https://www.baidu.com")
-print(response)    #返回请求结果(200)
-print(response.url)  # 返回请求地址
-print(response.text)  #返回网页
-print
+# print(response)    #返回请求响应码
+# print(response.url)  # 返回请求地址
+# print(response.text)  #返回网页
+# print(response.content.decode("utf-8"))  # 返回网页,并将字符编码转换成utf-8
 
+#常见字符集: UTF-8,GBK,GB2312,ASII,ISO-8859-1
+
+print(response.encoding)   # 获取返回的字符串编码 ISO-8859-1
+print(response.headers)    #h 获取返回的头部
+print(response.apparent_encoding)   # 返回字符编码
+
+print(response.content.decode(response.apparent_encoding))  #内容.编码为response获取的编码
+
+# 按utf-8打开本地html文件
+with open("baidu.html","w",encoding="utf-8") as f:
+    f.write(response.content.decode("utf-8"))
+
+# 用请求头把自己伪装成一个浏览器
+url = r"https://www.baidu.com"
+header = {"User-Agent:"
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+          }
+
+response = requests.get(url=url,header=header)
+
+# response = requests.get(url=url)
+print(response.content.decode((response.apparent_encoding)))
 
