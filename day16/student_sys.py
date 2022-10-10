0e0412c6aa5d10ea6252ae1905dccd69d665afee
ding <mydingyan@outlook.com>
2022-10-10 23:30:17 +0800

    学生管理系统

--- day16/student_sys.py
+++ day16/student_sys.py
@@ -9,15 +9,14 @@
 ## 创建一个学生系统，要有一个总体的规划
 
 
-
-# 菜单(print input)
+# 菜单(print input两个功能)
+from day16.util.delete_student import delete_name, delete_studentNo
+from day16.util.insert_student import insert_student  #在函数上，按住ait+回车，可以直接导入
+from day16.util.select_student import select_all_student, select_class_student, select_student_name, select_studentNo
+from day16.util.update_student import update_class, update_card
 
 
 # 打印菜单
-from day16.util.insert_student import insert_student
-from day16.util.select_student import select_all_student
-
-
 def menu():
     print("************主菜单*************")
     print("         1.新增学生记录")
@@ -33,25 +32,40 @@ def menu():
     print("******************************")
 
 if __name__ == '__main__':
-
     while True:
 
-        menu()
-        choice = int(input("请选择操作（0-9）："))
+        menu()  # 打印print菜单
+        choice = int(input("请选择操作(0-9):"))
 
-        if choice == 1:
+        if choice == 1:  #新增学生记录
             insert_student()
 
-        elif choice ==2:
+        elif choice == 2:  #列出所有学生
             select_all_student()
 
-        elif choice ==3:
+        elif choice == 3:  #按班级查询学生
+            select_class_student()
+
+        elif choice == 4:  #按姓名查询学生
+            select_student_name()
+
+        elif choice == 5:   #按学号查询学生
+            select_studentNo()
+
+        elif choice == 6:  #按姓名删除学生
+            delete_name()
+
+        elif choice == 7:  #按学号删除学生
+            delete_studentNo()
+
+        elif choice == 8:  #修改学生班级
+            update_class()
+
+        elif choice == 9:  #修改学生卡号
+            update_card()
+
+        elif choice == 0:
+            exit()  #退出操作
 
 
-        elif choice == 3:
-
-
-
-
-
-        else
+        # input("按回车继续...")
--- day16/util/select_student.py
+++ day16/util/select_student.py
@@ -5,20 +5,37 @@
 @time: 2022/10/7 13:18
 """
 
-
+from day16.database.mysql_normal import db   # day16.可以在外部调用
 
 def select_all_student():
-    all = db.select_all("students")
-    pass
+    all = db.select_all("students","age >0 ")
+    for one_s in all:
+        print(one_s)
 
 
 def select_class_student():  # 按班级查询
-    pass
+    class_stu = input("请输入要查询的班级编号:")
+    all = db.select_all("students",f"class = '{class_stu}班'")
+    if all:
+        for one_s in all:
+            print(one_s)
+    else:
+        print(f"没查到{class_stu}班的学生")
 
-def select_class_student():  # 按姓名查
-    pass
+def select_student_name():  # 按姓名查
+    name = input("请输入要查询的姓名:")
+    stu = db.select_all("students",f"name = '{name}'")
+    if stu:
+        for one_s in stu:
+            print(one_s)
+    else:
+        print(f"未查到{name}的信息")
 
 
 def select_studentNo():  # 按学号查
-    pass
-
+    sty_no = input("请输入要查询的学号:")
+    stu = db.select_one("students",f"studentNo = '{sty_no}'")
+    if stu:   #打印单行数据
+        print(stu)
+    else:
+        print(f"没查到{sty_no}学号的学生")
